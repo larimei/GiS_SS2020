4da936ac4c4fb6add9c2dc42396e986bd395c0a1
Lara <lara.maria.meister@hs-furtwangen.de>
2020-06-18 16:58:05 +0200

    Serveränderung

--- Aufgabe08/script.js
+++ Aufgabe08/script.js
@@ -7,8 +7,8 @@ var A08Server;
     console.log("Starting server");
     let port = Number(process.env.PORT);
     if (!port)
-        port = 8100;
-    let server = Http.createServer();
+        port = 8100; //port wird erstellt -> falls noch keiner vorhanden ist
+    let server = Http.createServer(); //Server wird erstellt mit 2 Listener
     server.addListener("request", handleRequest);
     server.addListener("listening", handleListen);
     server.listen(port);
@@ -17,6 +17,7 @@ var A08Server;
     }
     function handleRequest(_request, _response) {
         console.log("I hear voices!");
+        console.log(_request.url);
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
--- Aufgabe08/script.ts
+++ Aufgabe08/script.ts
@@ -4,19 +4,20 @@ export namespace A08Server {
   console.log("Starting server");
   let port: number = Number(process.env.PORT);
   if (!port)
-    port = 8100;
+    port = 8100;  //port wird erstellt -> falls noch keiner vorhanden ist
 
-  let server: Http.Server = Http.createServer();
-  server.addListener("request", handleRequest);
+  let server: Http.Server = Http.createServer();  //Server wird erstellt mit 2 Listener
+  server.addListener("request", handleRequest);    
   server.addListener("listening", handleListen);
   server.listen(port);
 
   function handleListen(): void {
-    console.log("Listening");
+    console.log("Listening"); 
   }
 
   function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
     console.log("I hear voices!");
+    console.log(_request.url);
 
     _response.setHeader("content-type", "text/html; charset=utf-8");
     _response.setHeader("Access-Control-Allow-Origin", "*");
