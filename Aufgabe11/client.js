8a87a15735633efe0376bd0d4d3dd21fc2208587
Lara <lara.maria.meister@hs-furtwangen.de>
2020-07-08 18:06:54 +0200

    Aufgabe11

--- Aufgabe11/client.js
+++ Aufgabe11/client.js
@@ -1,21 +1,32 @@
 "use strict";
 var Aufgabe11;
 (function (Aufgabe11) {
-    let buttonHTML = document.getElementById("buttonHTML");
-    buttonHTML.addEventListener("click", handleCommunicateHTML);
-    async function handleCommunicateHTML() {
+    let buttonSenden = document.getElementById("buttonSenden");
+    let buttonAuslesen = document.getElementById("buttonAuslesen");
+    buttonSenden.addEventListener("click", handleCommunicateSenden);
+    buttonAuslesen.addEventListener("click", handleCommunicateAuslesen);
+    async function handleCommunicateSenden() {
         let formData = new FormData(document.forms[0]);
-        let url = "https://gissose20.herokuapp.com/";
+        let url = "https://gissose20.herokuapp.com";
         // tslint:disable-next-line: no-any
         let query = new URLSearchParams(formData);
-        let antwort = await fetch(url + "html" + "?" + query.toString());
-        let antwortAusgabeURL = await antwort.url;
+        await fetch(url + "/senden" + "?" + query.toString());
+        console.log("Daten gesendet");
+    }
+    async function handleCommunicateAuslesen() {
+        let formData = new FormData(document.forms[0]);
+        let url = "https://gissose20.herokuapp.com";
+        // tslint:disable-next-line: no-any
+        let query = new URLSearchParams(formData);
+        let antwort = await fetch(url + "/auslesen" + "?" + query.toString());
         let antwortAusgabeText = await antwort.text();
-        let response = document.getElementById("Response");
+        let antwortUrl = await antwort.url;
+        let response = document.getElementById("response");
         let p = document.createElement("p");
         p.innerHTML = antwortAusgabeText;
         response.appendChild(p);
-        console.log(antwortAusgabeURL);
+        console.log(antwortAusgabeText);
+        console.log(antwortUrl);
     }
 })(Aufgabe11 || (Aufgabe11 = {}));
 //# sourceMappingURL=client.js.map
--- Aufgabe11/client.ts
+++ Aufgabe11/client.ts
@@ -1,23 +1,37 @@
 namespace Aufgabe11 {
-let buttonHTML: HTMLButtonElement = document.getElementById("buttonHTML") as HTMLButtonElement;
+let buttonSenden: HTMLButtonElement = document.getElementById("buttonSenden") as HTMLButtonElement;
+let buttonAuslesen: HTMLButtonElement = document.getElementById("buttonAuslesen") as HTMLButtonElement;
 
-buttonHTML.addEventListener("click", handleCommunicateHTML);
+buttonSenden.addEventListener("click", handleCommunicateSenden);
+buttonAuslesen.addEventListener("click", handleCommunicateAuslesen);
 
-async function handleCommunicateHTML(): Promise<void> {
+async function handleCommunicateSenden(): Promise<void> {
     let formData: FormData = new FormData(document.forms[0]);
-    let url: string = "https://gissose20.herokuapp.com/";
+    let url: string = "https://gissose20.herokuapp.com";
     // tslint:disable-next-line: no-any
     let query: URLSearchParams = new URLSearchParams(<any>formData);
-    let antwort: Response = await fetch(url + "html" + "?" + query.toString());
+    await fetch(url + "/senden" + "?" + query.toString());
 
-    let antwortAusgabeURL: string = await antwort.url;
+    console.log("Daten gesendet");
+}
+
+async function handleCommunicateAuslesen(): Promise<void> {
+    let formData: FormData = new FormData(document.forms[0]);
+    let url: string = "https://gissose20.herokuapp.com";
+    // tslint:disable-next-line: no-any
+    let query: URLSearchParams = new URLSearchParams(<any>formData);
+    let antwort: Response = await fetch(url + "/auslesen" + "?" + query.toString());
+
     let antwortAusgabeText: string = await antwort.text();
+    let antwortUrl: string = await antwort.url;
 
-    let response: HTMLElement = document.getElementById("Response") as HTMLDivElement;
+    let response: HTMLElement = document.getElementById("response") as HTMLDivElement;
     let p: HTMLElement = document.createElement("p");
     p.innerHTML = antwortAusgabeText;
     response.appendChild(p);
-    console.log(antwortAusgabeURL);
+
+    console.log(antwortAusgabeText);
+    console.log(antwortUrl);
 }
 
 
